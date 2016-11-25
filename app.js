646afa6d8ec6534553d2ee9bb1745d44cc24757b
clett1 <courtneylett2@gmail.com>
2016-11-25 16:35:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-/// <reference types="@argonjs/argon" />
-/// <reference types="three" />
-// grab some handles on APIs we use
-var Cesium = Argon.Cesium;
-var Cartesian3 = Argon.Cesium.Cartesian3;
-var ReferenceFrame = Argon.Cesium.ReferenceFrame;
-var JulianDate = Argon.Cesium.JulianDate;
-var CesiumMath = Argon.Cesium.CesiumMath;
+/// <reference path="../../typings/index.d.ts"/>
 // set up Argon
 var app = Argon.init();
+// set up THREE.  Create a scene, a perspective camera and an object
+// for the user's location
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera();
+var userLocation = new THREE.Object3D;
+scene.add(camera);
+scene.add(userLocation);
+// The CSS3DArgonRenderer supports mono and stereo views, and 
+// includes both 3D elements and a place to put things that appear 
+// fixed to the screen (heads-up-display) 
+var renderer = new THREE.CSS3DArgonRenderer();
+app.view.element.appendChild(renderer.domElement);
+// to easily control stuff on the display
+var hud = new THREE.CSS3DArgonHUD();
+// We put some elements in the index.html, for convenience. 
+// Here, we retrieve the description box and move it to the 
+// the CSS3DArgonHUD hudElements[0].  We only put it in the left
+// hud since we'll be hiding it in stereo
+var description = document.getElementById('description');
+hud.hudElements[0].appendChild(description);
+app.view.element.appendChild(hud.domElement);
 // Tell argon what local coordinate system you want.  The default coordinate
 // frame used by Argon is Cesium's FIXED frame, which is centered at the center
 // of the earth and oriented with the earth's axes.  
@@ -22,56 +36,96 @@ var app = Argon.init();
 // that here.  The other option Argon supports is localOriginEastNorthUp, which is
 // more similar to what is used in the geospatial industry
 app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
-// set up THREE.  Create a scene, a perspective camera and an object
-// for the user's location
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera();
-var userLocation = new THREE.Object3D;
-scene.add(camera);
-scene.add(userLocation);
-// In this demo, we are  rendering the 3D graphics with WebGL, 
-// using the standard WebGLRenderer
-var renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    logarithmicDepthBuffer: true
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-app.view.element.appendChild(renderer.domElement);
-// We put some elements in the index.html, for convenience. 
-var locationElement = document.getElementById("location");
-// All geospatial objects need to have an Object3D linked to a Cesium Entity.
-// We need to do this because Argon needs a mapping between Entities and Object3Ds.
-//
-// Here, we will position a cube near our starting location.  This geolocated object starts without a
-// location, until our reality is set and we know the location.  Each time the reality changes, we update
-// the cube position.
-// This code creates a 1m cube with a wooden box texture on it, 
-// that we will attach to the geospatial object when we create it.
-// Box texture from https://www.flickr.com/photos/photoshoproadmap/8640003215/sizes/l/in/photostream/
-// licensed under https://creativecommons.org/licenses/by/2.0/legalcode
-var boxGeoObject = new THREE.Object3D();
-var box = new THREE.Object3D();
-var loader = new THREE.TextureLoader();
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-
-/*
-loader.load('https://imgur.com/ADRyax1', function (texture) {
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ map: texture });
-    var mesh = new THREE.Mesh(geometry, material);
-    box.add(mesh);
-});*/
-boxGeoObject.add(box);
-var boxGeoEntity = new Argon.Cesium.Entity({
-    name: "I have a box",
-    position: Cartesian3.ZERO,
-    orientation: Cesium.Quaternion.IDENTITY
-});
+// creating 6 divs to indicate the x y z positioning
+var divXpos = document.createElement('div');
+var divXneg = document.createElement('div');
+var divYpos = document.createElement('div');
+var divYneg = document.createElement('div');
+var divZpos = document.createElement('div');
+var divZneg = document.createElement('div');
+// programatically create a stylesheet for our direction divs
+// and add it to the document
+var style = document.createElement("style");
+style.type = 'text/css';
+document.head.appendChild(style);
+var sheet = style.sheet;
+sheet.insertRule("\n    .cssContent {\n        opacity: 0.5;\n        width: 100px;\n        height: 100px;\n        border-radius: 50%;\n        line-height: 100px;\n        fontSize: 20px;\n        text-align: center;\n    }\n", 0);
+// Put content in each one  (should do this as a couple of functions)
+// for X
+divXpos.className = "cssContent";
+divXpos.style.backgroundColor = "red";
+divXpos.innerText = "Pos X = East";
+divXneg.className = "cssContent";
+divXneg.style.backgroundColor = "red";
+divXneg.innerText = "Neg X = West";
+// for Y
+divYpos.className = "cssContent";
+divYpos.style.backgroundColor = "blue";
+divYpos.innerText = "Pos Y = Up";
+divYneg.className = "cssContent";
+divYneg.style.backgroundColor = "blue";
+divYneg.innerText = "Neg Y = Down";
+//for Z
+divZpos.className = "cssContent";
+divZpos.style.backgroundColor = "green";
+divZpos.innerText = "Pos Z = South";
+divZneg.className = "cssContent";
+divZneg.style.backgroundColor = "green";
+divZneg.innerText = "Neg Z = North";
+// create 6 CSS3DObjects in the scene graph.  The CSS3DObject object 
+// is used by the CSS3DArgonRenderer. Because an HTML element can only
+// appear once in the DOM, we need two elements to create a stereo view.
+// The CSS3DArgonRenderer manages these for you, using the CSS3DObject.
+// You can pass a single DIV to the CSS3DObject, which
+// will be cloned to create a second matching DIV in stereo mode, or you
+// can pass in two DIVs in an array (one for the left and one for the 
+// right eyes).  If the content of the DIV does not change as the 
+// application runs, letting the CSS3DArgonRenderer clone them is 
+// simplest.  If it is changing, passing in two and updating both
+// yourself is simplest.
+var cssObjectXpos = new THREE.CSS3DObject(divXpos);
+var cssObjectXneg = new THREE.CSS3DObject(divXneg);
+var cssObjectYpos = new THREE.CSS3DObject(divYpos);
+var cssObjectYneg = new THREE.CSS3DObject(divYneg);
+var cssObjectZpos = new THREE.CSS3DObject(divZpos);
+var cssObjectZneg = new THREE.CSS3DObject(divZneg);
+// the width and height is used to align things.
+cssObjectXpos.position.x = 200.0;
+cssObjectXpos.position.y = 0.0;
+cssObjectXpos.position.z = 0.0;
+cssObjectXpos.rotation.y = -Math.PI / 2;
+cssObjectXneg.position.x = -200.0;
+cssObjectXneg.position.y = 0.0;
+cssObjectXneg.position.z = 0.0;
+cssObjectXneg.rotation.y = Math.PI / 2;
+// for Y
+cssObjectYpos.position.x = 0.0;
+cssObjectYpos.position.y = 200.0;
+cssObjectYpos.position.z = 0.0;
+cssObjectYpos.rotation.x = Math.PI / 2;
+cssObjectYneg.position.x = 0.0;
+cssObjectYneg.position.y = -200.0;
+cssObjectYneg.position.z = 0.0;
+cssObjectYneg.rotation.x = -Math.PI / 2;
+// for Z
+cssObjectZpos.position.x = 0.0;
+cssObjectZpos.position.y = 0.0;
+cssObjectZpos.position.z = 200.0;
+cssObjectZpos.rotation.y = Math.PI;
+cssObjectZneg.position.x = 0.0;
+cssObjectZneg.position.y = 0.0;
+cssObjectZneg.position.z = -200.0;
+//no rotation need for this one
+userLocation.add(cssObjectXpos);
+userLocation.add(cssObjectXneg);
+userLocation.add(cssObjectYpos);
+userLocation.add(cssObjectYneg);
+userLocation.add(cssObjectZpos);
+userLocation.add(cssObjectZneg);
 // the updateEvent is called each time the 3D world should be
 // rendered, before the renderEvent.  The state of your application
 // should be updated here.
-var boxInit = false;
-app.updateEvent.addEventListener(function (frame) {
+app.updateEvent.addEventListener(function () {
     // get the position and orientation (the "pose") of the user
     // in the local coordinate frame.
     var userPose = app.context.getEntityPose(app.context.user);
@@ -80,62 +134,55 @@ app.updateEvent.addEventListener(function (frame) {
     if (userPose.poseStatus & Argon.PoseStatus.KNOWN) {
         userLocation.position.copy(userPose.position);
     }
-    else {
-        // if we don't know the user pose we can't do anything
-        return;
-    }
-    // the first time through, we create a geospatial position for
-    // the box somewhere near us 
-    if (!boxInit) {
-        var defaultFrame = app.context.getDefaultReferenceFrame();
-        // set the box's position to 10 meters away from the user.
-        // First, clone the userPose postion, and add 10 to the X
-        var boxPos = userPose.position.clone();
-        boxPos.x += 10;
-        // set the value of the box Entity to this local position, by
-        // specifying the frame of reference to our local frame
-        boxGeoEntity.position.setValue(boxPos, defaultFrame);
-        // orient the box according to the local world frame
-        boxGeoEntity.orientation.setValue(Cesium.Quaternion.IDENTITY);
-        // now, we want to move the box's coordinates to the FIXED frame, so
-        // the box doesn't move if the local coordinate system origin changes.
-        if (Argon.convertEntityReferenceFrame(boxGeoEntity, frame.time, ReferenceFrame.FIXED)) {
-            scene.add(boxGeoObject);
-            boxInit = true;
-        }
-    }
-    // get the local coordinates of the local box, and set the THREE object
-    var boxPose = app.context.getEntityPose(boxGeoEntity);
-    boxGeoObject.position.copy(boxPose.position);
-    boxGeoObject.quaternion.copy(boxPose.orientation);
-    // rotate the boxes at a constant speed, independent of frame rates     
-    // to make it a little less boring
-    box.rotateY(3 * frame.deltaTime / 10000);
 });
-// renderEvent is fired whenever argon wants the app to update its display
+// for the CSS renderer, we want to use requestAnimationFrame to 
+// limit the number of repairs of the DOM.  Otherwise, as the 
+// DOM elements are updated, extra repairs of the DOM could be 
+// initiated.  Extra repairs do not appear to happen within the 
+// animation callback.
+var viewport = null;
+var subViews = null;
+var rAFpending = false;
 app.renderEvent.addEventListener(function () {
-    // set the renderers to know the current size of the viewport.
+    // only schedule a new callback if the old one has completed
+    if (!rAFpending) {
+        rAFpending = true;
+        viewport = app.view.getViewport();
+        subViews = app.view.getSubviews();
+        window.requestAnimationFrame(renderFunc);
+    }
+});
+// the animation callback.  
+function renderFunc() {
+    // if we have 1 subView, we're in mono mode.  If more, stereo.
+    var monoMode = subViews.length == 1;
+    rAFpending = false;
+    // set the renderer to know the current size of the viewport.
     // This is the full size of the viewport, which would include
     // both views if we are in stereo viewing mode
-    var viewport = app.view.getViewport();
     renderer.setSize(viewport.width, viewport.height);
-    // there is 1 subview in monocular mode, 2 in stereo mode    
-    for (var _i = 0, _a = app.view.getSubviews(); _i < _a.length; _i++) {
-        var subview = _a[_i];
-        var frustum = subview.frustum;
+    hud.setSize(viewport.width, viewport.height);
+    // there is 1 subview in monocular mode, 2 in stereo mode
+    for (var _i = 0, subViews_1 = subViews; _i < subViews_1.length; _i++) {
+        var subview = subViews_1[_i];
         // set the position and orientation of the camera for 
         // this subview
         camera.position.copy(subview.pose.position);
         camera.quaternion.copy(subview.pose.orientation);
         // the underlying system provide a full projection matrix
-        // for the camera. 
+        // for the camera.  Use it, and then update the FOV of the 
+        // camera from it (needed by the CSS Perspective DIV)
         camera.projectionMatrix.fromArray(subview.projectionMatrix);
+        camera.fov = subview.frustum.fovy * 180 / Math.PI;
         // set the viewport for this view
-        var _b = subview.viewport, x = _b.x, y = _b.y, width = _b.width, height = _b.height;
-        // set the webGL rendering parameters and render this view
-        renderer.setViewport(x, y, width, height);
-        renderer.setScissor(x, y, width, height);
-        renderer.setScissorTest(true);
-        renderer.render(scene, camera);
+        var _a = subview.viewport, x = _a.x, y = _a.y, width = _a.width, height = _a.height;
+        renderer.setViewport(x, y, width, height, subview.index);
+        // render this view.
+        renderer.render(scene, camera, subview.index);
+        // adjust the hud, but only in mono
+        if (monoMode) {
+            hud.setViewport(x, y, width, height, subview.index);
+            hud.render(subview.index);
+        }
     }
-});
+}
