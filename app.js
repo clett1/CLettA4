e3edcc7b388e2e40f783b9006fd8f9ef399226ef
clett1 <courtneylett2@gmail.com>
2016-11-25 17:06:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-/// <reference path="../../typings/index.d.ts"/>
-// set up Argon
+// Sets up Argon for the site
 var app = Argon.init();
-// set up THREE.  Create a scene, a perspective camera and an object
-// for the user's location
+
+// set up THREE.  Create a scene, a perspective camera and an object for the user's location
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera();
 var userLocation = new THREE.Object3D;
-scene.add(camera);
-scene.add(userLocation);
+
+//Add camera to the scene
+scene.add(camera);  
+
+//Add user location to the scene
+scene.add(userLocation); 
+
 // The CSS3DArgonRenderer supports mono and stereo views, and 
 // includes both 3D elements and a place to put things that appear 
 // fixed to the screen (heads-up-display) 
 var renderer = new THREE.CSS3DArgonRenderer();
 app.view.element.appendChild(renderer.domElement);
+
 // to easily control stuff on the display
 var hud = new THREE.CSS3DArgonHUD();
+
 // We put some elements in the index.html, for convenience. 
 // Here, we retrieve the description box and move it to the 
 // the CSS3DArgonHUD hudElements[0].  We only put it in the left
 // hud since we'll be hiding it in stereo
 var description = document.getElementById('description');
+
+//Add description element to hud
 hud.hudElements[0].appendChild(description);
+
 app.view.element.appendChild(hud.domElement);
+
 // Tell argon what local coordinate system you want.  The default coordinate
 // frame used by Argon is Cesium's FIXED frame, which is centered at the center
 // of the earth and oriented with the earth's axes.  
@@ -36,42 +46,28 @@ app.view.element.appendChild(hud.domElement);
 // that here.  The other option Argon supports is localOriginEastNorthUp, which is
 // more similar to what is used in the geospatial industry
 app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
+
 // creating 6 divs to indicate the x y z positioning
 var divXpos = document.createElement('div');
 var divXneg = document.createElement('div');
-var divYpos = document.createElement('div');
-var divYneg = document.createElement('div');
-var divZpos = document.createElement('div');
-var divZneg = document.createElement('div');
+
 // programatically create a stylesheet for our direction divs
 // and add it to the document
 var style = document.createElement("style");
 style.type = 'text/css';
 document.head.appendChild(style);
 var sheet = style.sheet;
-sheet.insertRule("\n    .cssContent {\n        opacity: 0.5;\n        width: 100px;\n        height: 100px;\n        border-radius: 50%;\n        line-height: 100px;\n        fontSize: 20px;\n        text-align: center;\n    }\n", 0);
+sheet.insertRule("\n.cssContent {\n opacity: 0.75;\n        width: 100px;\n        height: 100px;\n        border-radius: 10px;\n        line-height: 100px;\n        fontSize: 20px;\n        text-align: center;\n    }\n", 0);
+
 // Put content in each one  (should do this as a couple of functions)
 // for X
 divXpos.className = "cssContent";
-divXpos.style.backgroundColor = "red";
+divXpos.style.backgroundColor = "blue";
 divXpos.innerText = "Pos X = East";
 divXneg.className = "cssContent";
 divXneg.style.backgroundColor = "red";
 divXneg.innerText = "Neg X = West";
-// for Y
-divYpos.className = "cssContent";
-divYpos.style.backgroundColor = "blue";
-divYpos.innerText = "Pos Y = Up";
-divYneg.className = "cssContent";
-divYneg.style.backgroundColor = "blue";
-divYneg.innerText = "Neg Y = Down";
-//for Z
-divZpos.className = "cssContent";
-divZpos.style.backgroundColor = "green";
-divZpos.innerText = "Pos Z = South";
-divZneg.className = "cssContent";
-divZneg.style.backgroundColor = "green";
-divZneg.innerText = "Neg Z = North";
+
 // create 6 CSS3DObjects in the scene graph.  The CSS3DObject object 
 // is used by the CSS3DArgonRenderer. Because an HTML element can only
 // appear once in the DOM, we need two elements to create a stereo view.
@@ -85,43 +81,22 @@ divZneg.innerText = "Neg Z = North";
 // yourself is simplest.
 var cssObjectXpos = new THREE.CSS3DObject(divXpos);
 var cssObjectXneg = new THREE.CSS3DObject(divXneg);
-var cssObjectYpos = new THREE.CSS3DObject(divYpos);
-var cssObjectYneg = new THREE.CSS3DObject(divYneg);
-var cssObjectZpos = new THREE.CSS3DObject(divZpos);
-var cssObjectZneg = new THREE.CSS3DObject(divZneg);
+
 // the width and height is used to align things.
 cssObjectXpos.position.x = 200.0;
 cssObjectXpos.position.y = 0.0;
 cssObjectXpos.position.z = 0.0;
 cssObjectXpos.rotation.y = -Math.PI / 2;
+
 cssObjectXneg.position.x = -200.0;
 cssObjectXneg.position.y = 0.0;
 cssObjectXneg.position.z = 0.0;
 cssObjectXneg.rotation.y = Math.PI / 2;
-// for Y
-cssObjectYpos.position.x = 0.0;
-cssObjectYpos.position.y = 200.0;
-cssObjectYpos.position.z = 0.0;
-cssObjectYpos.rotation.x = Math.PI / 2;
-cssObjectYneg.position.x = 0.0;
-cssObjectYneg.position.y = -200.0;
-cssObjectYneg.position.z = 0.0;
-cssObjectYneg.rotation.x = -Math.PI / 2;
-// for Z
-cssObjectZpos.position.x = 0.0;
-cssObjectZpos.position.y = 0.0;
-cssObjectZpos.position.z = 200.0;
-cssObjectZpos.rotation.y = Math.PI;
-cssObjectZneg.position.x = 0.0;
-cssObjectZneg.position.y = 0.0;
-cssObjectZneg.position.z = -200.0;
-//no rotation need for this one
+
+
 userLocation.add(cssObjectXpos);
 userLocation.add(cssObjectXneg);
-userLocation.add(cssObjectYpos);
-userLocation.add(cssObjectYneg);
-userLocation.add(cssObjectZpos);
-userLocation.add(cssObjectZneg);
+
 // the updateEvent is called each time the 3D world should be
 // rendered, before the renderEvent.  The state of your application
 // should be updated here.
