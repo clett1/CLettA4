b0c2eca1cfee62907890045bac7df262ea849982
clett1 <courtneylett2@gmail.com>
2016-11-28 16:43:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 /// <reference path="../../typings/index.d.ts"/>
 // set up Argon
 var app = Argon.init();
@@ -61,7 +62,33 @@ divXpos.innerText = "Pos X = East";
 divXneg.className = "cssContent";
 divXneg.style.backgroundColor = "red";
 divXneg.innerText = "Neg X = West";
+*/
+var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
+			var renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+
+			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			var cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+			camera.position.z = 5;
+
+			var render = function () {
+				requestAnimationFrame( render );
+
+				cube.rotation.x += 0.1;
+				cube.rotation.y += 0.1;
+
+				renderer.render(scene, camera);
+			};
+
+			render();
+
+/*
 var cssObjectXpos = new THREE.CSS3DSprite(divXpos);
 //cssObjectXpos.position.x = 0;
 //cssObjectXpos.position.y = 0;
