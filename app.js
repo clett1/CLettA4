a7f29fcfecb3e0a97d6ebbd675a109961a4a46c1
clett1 <courtneylett2@gmail.com>
2016-11-28 23:53:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ var cssObjectArt = new THREE.CSS3DObject(artInfo);
 //cssObjectArt.position.x = 0;
 //cssObjectArt.position.y = 0;
 cssObjectArt.position.z = -.5;
-cssObjectArt.scale.set(.02, .02, .02);
+cssObjectArt.scale.set(.5, .5, .5);
 //cssObjectArt.rotation.y = -Math.PI / 2;
 //cssObjectArt.scale.set(1, 1, 1);
 var cssObjectCreator = new THREE.CSS3DSprite(creatorInfo);
