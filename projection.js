6a76ceb56a79a8bd85b3c028efab43305abb53e4
clett1 <courtneylett2@gmail.com>
2016-12-04 14:34:49 -0500

    Update projection.js

--- projection.js
+++ projection.js
@@ -163,10 +163,10 @@ projection.addView(fourthView);
 
 /*  This block of code contains the code for the Finite State Machine. This FSM controls AR Projections 
 */
-
+/*
 var startState = 'hidden';
 var currentState = ARProjectionFSM[startState];
-/*
+
 var ARProjectionFSM = {
     'hidden': {
         'targetseen': {
