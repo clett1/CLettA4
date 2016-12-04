3d2caacd5c60ba8b13956109b64bbe6822379892
clett1 <courtneylett2@gmail.com>
2016-12-03 21:41:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ app.vuforia.isAvailable().then(function (available) {
                         whenAvailable("twitterData", function(t) {
                             // do something
                             ARProjectionObject.add(cssObjectArt);
-                            console.log(twitterData);
+                            console.log("Twitter Data" + twitterData);
                           });
                         
                         
