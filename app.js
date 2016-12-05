fc671431527b70b46f047d4f38d66e40065e576e
clett1 <courtneylett2@gmail.com>
2016-12-04 22:42:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ scene.add(userLocation);
 var renderer = new THREE.CSS3DArgonRenderer();
 app.view.element.appendChild(renderer.domElement);
 
+var hud = new THREE.CSS3DArgonHUD();
+app.view.element.appendChild(hud.domElement);
 // Tell argon what local coordinate system you want.  The default coordinate
 // frame used by Argon is Cesium's FIXED frame, which is centered at the center
 // of the earth and oriented with the earth's axes.  
